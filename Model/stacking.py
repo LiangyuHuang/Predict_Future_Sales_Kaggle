514e735d9254573513625f8bf8b2c0ba7911d9af
Liangyu <liangyuhuang@outlook.com>
2020-07-24 20:56:00 +1000

    init

---
+++ Model/stacking.py
@@ -0,0 +1,7 @@
+class Stacking:
+    # https://blog.csdn.net/xiao2cai3niao/article/details/80571021
+
+
+
+
+
--- project.py
+++ project.py
@@ -1,5 +1,5 @@
 
-
+# https://github.com/1mrliu/AI_Learning_Competition/tree/master/HousePices
 if __name__ == '__main__':
     print('Hello Kaggle!')
     #data processing
@@ -10,4 +10,5 @@ if __name__ == '__main__':
 
 
     # model
+    # https://sklearn.apachecn.org/docs/master/1.html
     # https://blog.csdn.net/Irving_zhang/article/details/78561105
