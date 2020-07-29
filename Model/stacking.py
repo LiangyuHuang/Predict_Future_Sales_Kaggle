6b94d5684f9a8bf9e3341418b7f62bae68aef258
Liangyu <liangyuhuang@outlook.com>
2020-07-29 23:19:35 +1000

    Add stacking

--- Model/stacking.py
+++ Model/stacking.py
@@ -1,7 +1,18 @@
-class Stacking:
-    # https://blog.csdn.net/xiao2cai3niao/article/details/80571021
+import numpy as np
 
 
-
-
-
+def stacking(model, x_train, y_train, x_test, k_fold, num_k_fold):
+    stk_train = np.zeros((x_train.shape[0],))
+    stk_test = np.zeros((x_test.shape[0],))
+    stk_test_kf = np.zeros((num_k_fold, x_test.shape[0]))
+    i = 0
+    for train_index, test_index in k_fold.split(x_train):
+        kf_x_train = x_train[train_index]
+        kf_y_train = y_train[train_index]
+        model = model.fit(kf_x_train, kf_y_train)
+        kf_x_test = x_train[test_index]
+        stk_train[test_index] = model.predict(kf_x_test)
+        stk_test_kf[i, :] = model.predict(x_test)
+        i += 1
+    stk_test[:] = stk_test_kf.mean(axis=0)
+    return stk_train, stk_test
--- project.py
+++ project.py
@@ -1,131 +1,108 @@
-# from sklearn.kernel_ridge import KernelRidge
-from sklearn.linear_model import Lasso
+#!/usr/bin/python
+from sklearn.linear_model import Lasso, LinearRegression
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import RobustScaler
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.tree import DecisionTreeClassifier
 from math import sqrt
-import xgboost as xgb
-import lightgbm as lgb
+from xgboost import XGBRegressor
+from lightgbm import LGBMRegressor
 import time
 import pandas as pd
+import numpy as np
+import Model.stacking
 
-# https://github.com/1mrliu/AI_Learning_Competition/tree/master/HousePices
 if __name__ == '__main__':
-    print('Hello Kaggle!')
     # data processing
 
-
-
-
-
     # model
+    # initialize k-fold
+    num_k_fold = 5
+    kf = KFold(n_splits=num_k_fold)
 
     # initialize model
+    model = [
+        # regression
 
-    # regression
-    # Ridge
-    # cannot support
-    # model_ridge = KernelRidge(alpha=0.6, kernel='polynomial', degree=2, coef0=2.5)
-    # Lasso
-    model_lasso = make_pipeline(RobustScaler(), Lasso(alpha=0.0005, random_state=1))
+        # Linear
+        # LinearRegression(),
+        # rmse is too large
 
-    # classificaiton
+        # Lasso
+        make_pipeline(RobustScaler(), Lasso(alpha=0.0003, random_state=100)),
 
-    # bagging
-    # Random forest
-    # too slow
-    model_rf = RandomForestRegressor(n_estimators=3000,
-                                     criterion='mse',
-                                     max_features='sqrt',
-                                     min_samples_leaf=15,
-                                     min_samples_split=10,
-                                     max_depth=8)
+        # classificaiton
 
-    # boosting
-    # XGBoost
-    model_xgb = xgb.XGBRegressor(tree_method='hist',
-                                 max_depth=8,
-                                 n_estimators=3000,
-                                 min_child_weight=300,
-                                 colsample_bytree=0.8,
-                                 subsample=0.8,
-                                 eta=0.3,
-                                 seed=42)
-    # GDT
-    # too slow
-    model_gdt = GradientBoostingRegressor(n_estimators=3000, learning_rate=0.05,
-                                          max_depth=8, max_features='sqrt',
-                                          min_samples_leaf=15, min_samples_split=10,
-                                          loss='huber', random_state=5)
-
-    # LightGBM
-    model_lgb = lgb.LGBMRegressor(objective='regression', num_leaves=5,
-                                  learning_rate=0.05, n_estimators=3000,
-                                  max_bin=55, bagging_fraction=0.8,
-                                  bagging_freq=5, feature_fraction=0.2319,
-                                  feature_fraction_seed=9, bagging_seed=9,
-                                  min_data_in_leaf=6, min_sum_hessian_in_leaf=11,
-                                  max_depth=8)
+        # bagging
+        # Random forest
+        # too slow
+        RandomForestRegressor(n_estimators=3000,
+                              criterion='mse',
+                              max_features='sqrt',
+                              min_samples_leaf=15,
+                              min_samples_split=10,
+                              max_depth=8),
+        # boosting
+        # XGBoost
+        XGBRegressor(tree_method='hist',
+                     max_depth=8,
+                     n_estimators=3000,
+                     min_child_weight=300,
+                     colsample_bytree=0.8,
+                     subsample=0.8,
+                     eta=0.3,
+                     seed=42),
+        # GDT
+        # too slow
+        GradientBoostingRegressor(n_estimators=3000, learning_rate=0.005,
+                                  max_depth=8, max_features='sqrt',
+                                  min_samples_leaf=10, min_samples_split=10,
+                                  loss='huber', random_state=100),
+        # LightGBM
+        LGBMRegressor(objective='regression', num_leaves=8,
+                      learning_rate=0.005, n_estimators=3000,
+                      max_bin=55, bagging_fraction=0.8,
+                      bagging_freq=5, feature_fraction=0.2319,
+                      feature_fraction_seed=9, bagging_seed=9,
+                      min_data_in_leaf=6, min_sum_hessian_in_leaf=11,
+                      max_depth=8),
+    ]
 
     # import data
-    train_data = pd.read_csv('Data/train_finalv1_1.csv')
+    train_data = pd.read_csv('Data/train_finalv1_2.csv')
     train_data = train_data.drop(axis=1, columns='ID', inplace=False)
 
-    X_train = train_data.drop(axis=1, columns='SalePrice', inplace=False)[:1000]
-    Y_train = train_data['SalePrice'][:1000]
+    X_data = train_data.drop(axis=1, columns='SalePrice', inplace=False).values
+    Y_data = train_data['SalePrice'].values
 
-    # test_data = pd.read_csv('Data/test_fianlv1_1.csv')
-    # test_data = test_data.drop(axis=1, columns='ID', inplace=False)
+    X_train, X_valid, Y_train, Y_valid = train_test_split(X_data, Y_data, test_size=0.3, random_state=100)
 
-    X_valid = train_data.drop(axis=1, columns='SalePrice', inplace=False)[1000:]
-    Y_valid = train_data['SalePrice'][1000:]
-    # X_valid = test_data.drop(axis=1, columns='SalePrice', inplace=False)
-    # Y_valid = test_data['SalePrice']
+    # fitting and check the performance of each model
+    for regression in model:
+        tc = time.time()
+        regression.fit(X_train, Y_train)
+        Y_pred = regression.predict(X_valid)
+        r_mse = sqrt(mean_squared_error(Y_valid, Y_pred))
+        print('model\'s RMSE :', r_mse)
+        tc = time.time() - tc
+        print('time:', tc)
 
-    # fitting
+    tc = time.time()
+    new_x_train = np.zeros((X_train.shape[0], len(model)))
+    new_x_test = np.zeros((X_valid.shape[0], len(model)))
+    i = 0
+    for regression in model:
+        new_x_train[:, i], new_x_test[:, i] = Model.stacking.stacking(
+            regression, X_train, Y_train, X_valid, kf, num_k_fold)
+        i += 1
 
-    # model_ridge.fit(X_train, Y_train)
-    # Y_pred = model_ridge.predict(X_valid)
-    # accuracy_score(Y_valid, Y_pred)
-    # print ('model_ridge\'s score :', clf)
-
-    ts = time.time()
-    model_lasso.fit(X_train, Y_train)
-    Y_pred = model_lasso.predict(X_valid)
-    rmse = sqrt(mean_squared_error(Y_valid, Y_pred))
-    print('model_lasso\'s rmse :', rmse)
-    ts = time.time() - ts
-    print('time:', ts)
-
-    ts = time.time()
-    model_rf.fit(X_train, Y_train)
-    Y_pred = model_rf.predict(X_valid)
-    rmse = sqrt(mean_squared_error(Y_valid, Y_pred))
-    print('model_rf\'s rmse :', rmse)
-    ts = time.time() - ts
-    print('time:', ts)
-
-    ts = time.time()
-    model_xgb.fit(X_train, Y_train)
-    Y_pred = model_xgb.predict(X_valid)
-    rmse = sqrt(mean_squared_error(Y_valid, Y_pred))
-    print('model_xgb\'s rmse :', rmse)
-    ts = time.time() - ts
-    print('time:', ts)
-
-    ts = time.time()
-    model_gdt.fit(X_train, Y_train)
-    Y_pred = model_gdt.predict(X_valid)
-    rmse = sqrt(mean_squared_error(Y_valid, Y_pred))
-    print('model_gdt\'s rmse :', rmse)
-    ts = time.time() - ts
-    print('time:', ts)
-
-    ts = time.time()
-    model_lgb.fit(X_train, Y_train)
-    Y_pred = model_lgb.predict(X_valid)
-    rmse = sqrt(mean_squared_error(Y_valid, Y_pred))
-    print('model_lgb\'s rmse :', rmse)
-    ts = time.time() - ts
-    print('time:', ts)
+    # 调参
+    stacking = LinearRegression()
+    stacking.fit(new_x_train, Y_train)
+    Y_reg_pred = stacking.predict(new_x_test)
+    r_mse = sqrt(mean_squared_error(Y_valid, Y_reg_pred))
+    print('stacking model\'s RMSE :', r_mse)
+    tc = time.time() - tc
+    print('time:', tc)
